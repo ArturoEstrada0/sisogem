17032649138c55e4a5ead37c72a5bbf1d44219b7
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2023-11-01 18:25:31 -0600

    Login Change Email, Password

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Dashboard from "./components/Dashboard";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 
+import { Account } from "./components/Account";
+import SignUp from "./components/SignUp";
+import Login2 from "./components/Login";
+import Status from "./components/Status";
+import Settings from "./components/Settings";
+
 function App() {
   return (
     <Router>
       <div>
         <Header />
-        <Dashboard />
+        {/* <Dashboard /> */}
+        <Account>
+          <Status />
+          <SignUp />
+          <Login2 />
+          <Settings />
+        </Account>
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,22 @@
-import React, { useState } from "react";
-import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
-import UserPool from "../UserPool";
+import React, { useState, useContext } from "react";
+import { AccountContext } from "./Account";
 
 const Login2 = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { authenticate } = useContext(AccountContext);
+
   const onSubmit = (event) => {
     event.preventDefault();
 
-    const user = new CognitoUser({
-        Username: email,
-        Pool: UserPool,
+    authenticate(email, password)
+      .then((data) => {
+        console.log("Logged in!", data);
+      })
+      .catch((err) => {
+        console.error("Failed to login", err);
       });
-      
-
-    const authDetails = new AuthenticationDetails({
-      Username: email,
-      Password: password,
-    });
-
-    user.authenticateUser(authDetails, {
-      onSuccess: (data) => {
-        console.log("onSuccess: ", data);
-      },
-      onFailure: (err) => {
-        console.error("onFailure: ", err);
-      },
-      newPasswordRequired: (data) => {
-        console.log("newPasswordRequired: " , data);
-      },
-    });
   };
 
   return (
@@ -51,6 +37,6 @@ const Login2 = () => {
       </form>
     </div>
   );
-}
+};
 
 export default Login2;
