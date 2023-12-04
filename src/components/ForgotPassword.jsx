da0c9abc136b9a56172ab326329adc7d9d6df5da
Roytrexx <133319533+Roytrexx@users.noreply.github.com>
2023-12-04 01:47:27 -0600

    carrusel de fotos

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
-import Fondo from "../assets/img/acue4.jpg";
+import Fondo1 from "../assets/img/acue4.jpg";
+import Fondo2 from "../assets/img/morelia.jpg";
+import Fondo3 from "../assets/img/tara3.jpg";
+import Fondo4 from "../assets/img/sanD.jpg";
 import Icono from "../assets/img/logo_mich.png";
 import Escudo from "../assets/img/image.png";
-import './ForgotPassword.css';
+import "./ForgotPassword.css";
 
 const ForgotPassword = ({ changeView }) => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [showEmptyFieldsAlert, setShowEmptyFieldsAlert] = useState(false);
+  const [currentBackground, setCurrentBackground] = useState(0);
+
+  const backgrounds = [Fondo1, Fondo2, Fondo3, Fondo4]; // Agrega más imágenes según sea necesario
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentBackground((prev) => (prev + 1) % backgrounds.length);
+    }, 5000); // Cambia la imagen cada 5 segundos (ajusta según sea necesario)
+
+    return () => clearInterval(timer);
+  }, [backgrounds.length]);
 
   const handleForgotPassword = async (event) => {
     event.preventDefault();
@@ -22,11 +36,15 @@ const ForgotPassword = ({ changeView }) => {
 
     try {
       await Auth.forgotPassword(email);
-      setMessage("Se ha enviado un enlace de recuperación de contraseña a tu email.");
+      setMessage(
+        "Se ha enviado un enlace de recuperación de contraseña a tu email."
+      );
       // Aquí puedes cambiar la vista para que el usuario ingrese el código y la nueva contraseña
       changeView("resetPassword");
     } catch (error) {
-      setMessage("Hubo un error al intentar enviar el enlace de recuperación. Por favor, intenta de nuevo.");
+      setMessage(
+        "Hubo un error al intentar enviar el enlace de recuperación. Por favor, intenta de nuevo."
+      );
       console.error("Error al enviar el enlace de recuperación:", error);
     }
   };
@@ -69,7 +87,7 @@ const ForgotPassword = ({ changeView }) => {
               {message && <p className="message">{message}</p>}
               {showEmptyFieldsAlert && (
                 <div className="alert alert-danger" role="alert">
-                  ¡Por favor, proporciona tu correo electronico!
+                  ¡Por favor, proporciona tu correo electrónico!
                 </div>
               )}
               <div>
@@ -84,7 +102,16 @@ const ForgotPassword = ({ changeView }) => {
           </div>
         </div>
         <div className="col-md-8">
-          <img src={Fondo} alt="Fondo" className="img-forgot" />
+          {backgrounds.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Fondo ${index + 1}`}
+              className={`img-forgot ${
+                index === currentBackground ? "img-forgot-active" : ""
+              }`}
+            />
+          ))}
           <img src={Escudo} alt="Fondo" className="img-fore" />
         </div>
       </div>
