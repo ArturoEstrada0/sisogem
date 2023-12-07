e962dacc14479472b25c3805a4a5a084a35720a2
ArturoEstrada0 <116518245+ArturoEstrada0@users.noreply.github.com>
2023-12-07 11:49:19 -0600

    RolService

--- src/services/RolService.js
+++ src/services/RolService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3001'
+const API_URL = 'https://api-sisogem2.onrender.com'
 
 export class RolService {
   static async getAllRoles(userEmail) {
